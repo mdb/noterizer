e6903e8781057c700eb0deb837a5a08f42efee6d
Mike Ball <mikedball@gmail.com>
2014-07-20 10:56:58 -0400

    created basic notes scaffolding

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,12 @@
 class NotesController < ApplicationController
+  before_filter :force_json
+
+  def index
+  end
+
+  private
+
+  def force_json
+    request.format = :json
+  end
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe NotesController, :type => :controller do
+  before :each do
+    get :index
+  end
 
+  describe '#index' do
+    it 'successfully responds to requests' do
+      expect(response).to be_success
+    end
+
+    it 'returns JSON' do
+      expect(response.content_type).to eq 'application/json'
+    end
+  end
 end
