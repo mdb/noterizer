f91c925683649e790caeccd204de9d1fab193876
Mike Ball <mikedball@gmail.com>
2014-07-20 14:53:56 -0400

    added _note tests

---
+++ spec/views/notes/_note.json.jbuilder_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe 'notes/_note' do
+  let(:note) do
+    double('Note',
+      to: 'Mike',
+      from: 'Sam',
+      heading: 'Tomorrow',
+      body: 'Call me after 3pm.',
+    )
+  end
+
+  before :each do
+    assign(:note, note)
+
+    render '/notes/note', note: note
+  end
+
+  context 'verifying the JSON values it renders' do
+    subject { JSON.parse(rendered) }
+
+    describe "['toField']" do
+      subject { super()['toField'] }
+
+      it { is_expected.to eq 'Mike' }
+    end
+
+    describe "['fromField']" do
+      subject { super()['fromField'] }
+
+      it { is_expected.to eq 'Sam' }
+    end
+
+    describe "['heading']" do
+      subject { super()['heading'] }
+
+      it { is_expected.to eq 'Tomorrow' }
+    end
+
+    describe "['body']" do
+      subject { super()['body'] }
+
+      it { is_expected.to eq 'Call me after 3pm.' }
+    end
+  end
+end
